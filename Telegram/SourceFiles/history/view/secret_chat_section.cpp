812ef58319ca9f0004761d688b92df547e132683
Juchi <24779257+KebabLord@users.noreply.github.com>
2026-03-19 15:57:30 +0300

    cosmetic improvements

--- Telegram/SourceFiles/history/view/secret_chat_section.cpp
+++ Telegram/SourceFiles/history/view/secret_chat_section.cpp
@@ -15,6 +15,7 @@
 
 #include "styles/style_chat.h"
 #include "styles/style_chat_helpers.h"
+#include "styles/style_dialogs.h"
 #include "styles/style_settings.h"
 
 #include "rpl/filter.h"
@@ -23,6 +24,32 @@
 #include <lang_auto.h>
 
 namespace HistoryView {
+
+namespace {
+
+const auto kSecretChatNameColor = QColor(0x4E, 0xAD, 0x41);
+
+void PaintTintedIcon(
+		Painter &p,
+		const style::icon &icon,
+		QPoint position,
+		const QColor &color) {
+	const auto ratio = style::DevicePixelRatio();
+	auto image = QImage(
+		QSize(icon.width() * ratio, icon.height() * ratio),
+		QImage::Format_ARGB32_Premultiplied);
+	image.fill(Qt::transparent);
+	image.setDevicePixelRatio(ratio);
+	{
+		Painter q(&image);
+		icon.paint(q, QPoint(0, 0), icon.width());
+		q.setCompositionMode(QPainter::CompositionMode_SourceIn);
+		q.fillRect(QRect(QPoint(0, 0), QSize(icon.width(), icon.height())), color);
+	}
+	p.drawImage(position, image);
+}
+
+} // namespace
 
 SecretChatMemento::SecretChatMemento(int64_t chatId)
 : _chatId(chatId) {
@@ -66,6 +93,7 @@ SecretChatWidget::SecretChatWidget(
 		tr::lng_message_ph()))
 , _send(std::make_shared<Ui::SendButton>(this, st::historySend)) {
 	_title->setAttribute(Qt::WA_TransparentForMouseEvents);
+	_title->setTextColorOverride(kSecretChatNameColor);
 	_status->setAttribute(Qt::WA_TransparentForMouseEvents);
 	InitMessageField(controller, _field, nullptr);
 	_send->show();
@@ -145,8 +173,11 @@ void SecretChatWidget::checkActivation() {
 
 void SecretChatWidget::resizeEvent(QResizeEvent *e) {
 	SectionWidget::resizeEvent(e);
-	_title->resizeToNaturalWidth(width() - st::previewTop.namePosition.x());
-	_title->move(st::previewTop.namePosition);
+	const auto titleLeft = st::previewTop.namePosition.x()
+		+ st::dialogsUnlockIcon.width()
+		+ st::dialogsChatTypeSkip;
+	_title->resizeToNaturalWidth(width() - titleLeft);
+	_title->move(titleLeft, st::previewTop.namePosition.y());
 	_status->resizeToNaturalWidth(width() - st::previewTop.statusPosition.x());
 	_status->move(st::previewTop.statusPosition);
 	const auto composeHeight = _field->height() + 2 * st::historySendPadding;
@@ -171,6 +202,11 @@ void SecretChatWidget::paintEvent(QPaintEvent *e) {
 		e->rect());
 	p.fillRect(0, height() - st::historySendSize.height(), width(), st::historySendSize.height(), st::historyComposeAreaBg);
 	p.fillRect(0, 0, width(), st::previewTop.height, st::topBarBg);
+	PaintTintedIcon(
+		p,
+		st::dialogsUnlockIcon,
+		st::previewTop.namePosition,
+		kSecretChatNameColor);
 	p.fillRect(0, st::previewTop.height, width(), st::lineWidth, st::shadowFg);
 	p.fillRect(0, height() - st::historySendSize.height() - st::lineWidth, width(), st::lineWidth, st::shadowFg);
 }
